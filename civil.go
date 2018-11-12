01e28ce941a06456547eb32f92f3f7ed56c8e5b0
saward <mark.saward@episub.com>
2018-11-13 00:13:23 +1100

    fixed scanning for date and nulldate

--- civil.go
+++ civil.go
@@ -3,12 +3,33 @@ package pqt
 import (
 	"database/sql/driver"
 	"fmt"
-	"log"
-	"reflect"
 	"time"
 
 	"cloud.google.com/go/civil"
 )
+
+type Date struct {
+	civil.Date
+}
+
+// Scan implements the Scanner interface.
+func (d *Date) Scan(value interface{}) error {
+	var when time.Time
+	var valid bool
+	when, valid = value.(time.Time)
+	if !valid {
+		return fmt.Errorf("Not a date object")
+	}
+
+	d.Date = civil.DateOf(when)
+
+	return nil
+}
+
+// Value implements the driver Valuer interface.
+func (d Date) Value() (driver.Value, error) {
+	return d.Date.String(), nil
+}
 
 type NullDate struct {
 	Date  civil.Date
@@ -17,21 +38,14 @@ type NullDate struct {
 
 // Scan implements the Scanner interface.
 func (nd *NullDate) Scan(value interface{}) error {
-	if value == nil {
-		nd.Valid = false
+	var when time.Time
+	when, nd.Valid = value.(time.Time)
+	if !nd.Valid {
 		return nil
 	}
 
-	ts := reflect.TypeOf(value).String()
-	if ts != "time.Time" {
-		return fmt.Errorf("Unexpected type for NullDate of %s", ts)
-	}
-
-	v, _ := value.(time.Time)
-	nd.Date = civil.DateOf(v)
+	nd.Date = civil.DateOf(when)
 	nd.Valid = true
-
-	log.Printf("Time: %s", v.Format(time.RFC3339))
 
 	return nil
 }
